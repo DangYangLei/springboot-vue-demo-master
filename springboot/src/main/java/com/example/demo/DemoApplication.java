be86cdadebb75ef6651c2c94c0862f53f10c1077
dyl <923667775@qq.com>
2022-09-14 09:35:25 +0800

    02 Commit

--- springboot/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import java.sql.Time;
+
 @SpringBootApplication
 @EnableWebSecurity   //开启security
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
 
+	}
 }
